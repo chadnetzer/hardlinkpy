5e9d961212d9d04679bfd3a94a35cbac0c6b15a7
wolfospealain <eoinospealain@gmail.com>
2018-07-02 18:30:08 +0100

    Update file hashes following hardlink

--- hardlink.py
+++ hardlink.py
@@ -247,18 +247,20 @@ def hardlink_identical_files(filename, stat_info, options):
             # We did not find this file as hardlinked to any other file
             # yet.  So now lets see if our file should be hardlinked to any
             # of the other files with the same hash.
-            for (temp_filename, temp_stat_info) in file_hashes[file_hash]:
+            for index, (temp_filename, temp_stat_info) in enumerate(file_hashes[file_hash]):
                 if are_files_hardlinkable(work_file_info, (temp_filename, temp_stat_info),
                                           options):
                     # Find the attributes of the newest file
-                    update_stat_info = os.stat(temp_filename)
+                    update_stat_info = temp_stat_info
                     if stat_info.st_mtime > update_stat_info.st_mtime:
                         update_stat_info = stat_info
                     # Always use the file with the most hardlinks as the source
-                    if (stat_info.st_nlink > temp_stat_info.st_nlink):
+                    if stat_info.st_nlink > temp_stat_info.st_nlink:
                         hardlink_files(filename, temp_filename, update_stat_info, options)
                     else:
                         hardlink_files(temp_filename, filename, update_stat_info, options)
+                    # Update files hashes
+                    file_hashes[file_hash][index] = (filename, os.stat(filename))
                     break
             else:
                 # The file should NOT be hardlinked to any of the other
