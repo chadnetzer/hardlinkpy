87f886d056b94d7b9b2d5356a714c71d996b63ee
Chad Netzer <chad.netzer@gmail.com>
2018-07-09 06:37:24 -0700

    Updated setup.py
    Still need to work on long description, and possibly other fields.

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-setup(name='hardlinkpy',
-      version='0.5',
-      description='Filesystem deduplification',
-      author='John Villalovos',
-      author_email='john@sodarock.com',
-      py_modules=["hardlink"],
+setup(name='hardlinkable',
+      version='0.8',
+      description='Find identical files in directory trees and optionally hard-link them',
+      author='Chad Netzer',
+      author_email='chad.netzer+hardlinkable@gmail.com',
+      py_modules=["hardlinkable"],
       test_suite="tests",
       entry_points={
-          'console_scripts': ['hardlink=hardlink.main']
-      })
+          'console_scripts': ['hardlinkable=hardlinkable.main']
+      },
+      classifiers=(
+          "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 3",
+          "Operating System :: POSIX",
+          "Operating System :: MacOS",
+          "Operating System :: MacOS :: MacOS X",
+          "Operating System :: Unix",
+      ),
+)
