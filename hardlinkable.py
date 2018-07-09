19e3e0c1621785bee3ae9752dc21f30820d80317
Chad Netzer <chad.netzer@gmail.com>
2018-07-09 04:25:31 -0700

    Remove old printversion function (no longer used)
    This function is from before the change to optparse.  The copyright info
    is redundant (and hadn't been updated), so should be deleted.

--- hardlinkable.py
+++ hardlinkable.py
@@ -382,26 +382,6 @@ def humanize_number(number):
     return ("%d bytes" % number)
 
 
-def printversion(self):
-    print("hardlink.py, Version %s" % VERSION)
-    print("Copyright 2007 - 2018  Chad Netzer and contributors")
-    print("Copyright 2003 - 2018  John L. Villalovos.")
-    print("""
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation; either version 2 of the License, or (at your option) any later
-version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with
-this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-Place, Suite 330, Boston, MA  02111-1307, USA.
-""")
-
-
 def parse_command_line():
     usage = "usage: %prog [options] directory [ directory ... ]"
     version = "%prog: " + VERSION
