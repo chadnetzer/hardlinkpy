f8d1cfec105802a2ad631e125bc9107654be51b5
Chad Netzer <chad.netzer@gmail.com>
2018-07-19 06:16:45 -0700

    Change shebang to use /usr/bin/env python
    Supported on both python 2 (even back to 2.3), as well as python 3.

--- hardlinkable.py
+++ hardlinkable.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 # hardlinkable - Goes through a directory structure and reports files which are
 # identical and could be hard-linked together.  Optionally performs the
